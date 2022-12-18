57bc010a4ab9c2177188486a33ae8527688d2f31
Sudharshan Kumar Mailabattula <siddumail61@gmail.com>
2022-12-19 00:04:53 +0530

    2nd time commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
 
+        System.out.println("Let's see how this goes...!");
+
+        NewFile01 book1 = new NewFile01();
+        book1.fileName = "Harry Potter and the Sorcerer Stone.";
+        book1.indexNum = 23;
+        book1.utility = true;
+
+        NewFile01 book3 = new NewFile01("Harry Potter and the prisoner of Azkaban", 45, true);
+
+        System.out.println(book1.fileName);
     }
 }
--- src/NewFile01.java
+++ src/NewFile01.java
@@ -1,5 +1,16 @@
 public class NewFile01 {
 
+    String fileName;
+    int indexNum;
+    boolean utility;
 
+    public NewFile01() {
+
+    }
+    public NewFile01(String fileName, int indexNum, boolean utility) {
+        this.fileName = fileName;
+        this.indexNum = indexNum;
+        this.utility = utility;
+    }
 
 }
